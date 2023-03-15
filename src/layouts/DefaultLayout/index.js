90152cf81bddc299bf4ef69a15faf98f4763124d
Viet Truong <viettruong0825@gmail.com>
2023-03-15 16:01:15 +0700

    install classnames, define layout width and write GlobalStyles

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,11 +1,13 @@
-import { Children } from "react";
-import Header from "../components/Header";
+import styles from "./DefaultLayout.module.scss";
+import Header from "../components/Header/Header";
+import classNames from "classnames/bind";
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div className={"wrapper"}>
+        <div className={cx("wrapper")}>
             <Header />
-            <div className={"container"}>{children}</div>
+            <div className={cx("container")}>{children}</div>
         </div>
     );
 }
