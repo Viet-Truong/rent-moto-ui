8d505f139d18b3ed39135257a1d32371bb72245e
Viet Truong <viettruong0825@gmail.com>
2023-04-03 22:13:38 +0700

    add Data picker

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,27 @@ import "./Home.css";
 import Slider from "~/components/Slider";
 import { slider_data } from "~/data/slide";
 import { policy } from "~/data/data";
-import { DateRangePicker } from "rsuite";
+import { DatePicker } from "antd";
+const { RangePicker } = DatePicker;
 
 const cx = classNames.bind(styles);
 function Home() {
-    const { combine, allowedMaxDays, beforeToday } = DateRangePicker;
+    // const setFilter = (values) => {
+    //     if (values) {
+    //         if (values.length > 1) {
+    //             // var selectedFrom = moment(values[0], "MMM DD yyyy HH:mm");
+    //             // var selectedTo = moment(values[1], "MMM DD yyyy HH:mm");
+    //             setFrom(moment(values[0]).format("MMM DD yyyy HH"));
+    //             setTo(moment(values[1]).format("MMM DD yyyy HH"));
+    //             var selectedFrom = moment(new Date(values[0]._d)).format(
+    //                 "MMM DD yyyy HH:mm"
+    //             );
+    //             var selectedTo = moment(new Date(values[1]._d)).format(
+    //                 "MMM DD yyyy HH:mm"
+    //             );
+    //         }
+    //     }
+    // };
 
     return (
         <div className={cx("home")}>
@@ -36,18 +52,11 @@ function Home() {
                     <h2 className={cx("date-picker__title")}>
                         Vui lòng chọn ngày bắt đầu và ngày kết thúc
                     </h2>
-                    <DateRangePicker
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            maxWidth: "500px",
-                            marginTop: "50px",
-                        }}
-                        disabledDate={combine(
-                            allowedMaxDays(31),
-                            beforeToday()
-                        )}
+                    <RangePicker
+                        className="RangePicker"
+                        format="DD MMM yyyy"
+                        // onChange={setFilter}
+                        style={{ height: "3.5rem", width: "37rem" }}
                     />
                 </div>
             </div>
