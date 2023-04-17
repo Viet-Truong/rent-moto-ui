ff5bc509da46f3b602830b1667be9b88e8337dc3
Viet Truong <viettruong0825@gmail.com>
2023-04-17 23:41:33 +0700

    add ui register

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import config from "~/config";
 import MotoDetail from "~/pages/MotoDetail";
 import Home from "~/pages/Home/Home";
 import Login from "~/pages/Login/Login";
+import Register from "~/pages/Register/Register";
 
 // public routes
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.moto, component: MotoDetail },
     { path: config.routes.login, component: Login, layout: null },
+    { path: config.routes.register, component: Register, layout: null },
 ];
 
 export { publicRoutes };
