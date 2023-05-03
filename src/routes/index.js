d66fe65d8f69b932ae25c5a30056654278ac1451
Viet Truong <viettruong0825@gmail.com>
2023-05-03 18:04:24 +0700

    fix name file

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Home from "~/pages/Home/Home";
 import Login from "~/pages/Login/Login";
 import Register from "~/pages/Register/Register";
 import HomeAdmin from "~/pages/Admin/HomeAdmin";
+import Profile from "~/pages/Profile/Profile";
 
 // public routes
 const publicRoutes = [
