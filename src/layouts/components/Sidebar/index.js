8dd06d770962c761dcdca5eb364a966b3b84f0d3
Viet Truong <viettruong0825@gmail.com>
2023-05-08 23:39:14 +0700

    add icon nav of sidebar

--- src/layouts/components/Sidebar/index.js
+++ src/layouts/components/Sidebar/index.js
@@ -2,6 +2,17 @@ import classNames from "classnames/bind";
 import config from "~/config";
 import Menu, { MenuItem } from "./Menu";
 import styles from "./Sidebar.module.scss";
+import image from "~/assets/image";
+import Image from "~/components/Image";
+import { Link } from "react-router-dom";
+import {
+    faUsers,
+    faUser,
+    faMotorcycle,
+    faRotateLeft,
+    faCheckCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const cx = classNames.bind(styles);
 
@@ -9,36 +20,48 @@ function Sidebar() {
     return (
         <aside className={cx("wrapper")}>
             <div className={cx("wrapper-fix")}>
+                <div className={cx("header")}>
+                    <div className={cx("logo")}>
+                        <Link to={config.routes.home} className={"logo-link"}>
+                            <Image src={image.logo} alt="Website logo" />
+                        </Link>
+                    </div>
+                    <Image
+                        className={cx("user-avatar")}
+                        src={""}
+                        alt={"avatar"}
+                    />
+                </div>
                 <Menu>
                     <MenuItem
                         title={"Quản lí tài khoản"}
                         to={config.routes.admin + "/managerAccount"}
-                        icon={""}
-                        activeIcon={""}
+                        icon={<FontAwesomeIcon icon={faUsers} />}
+                        activeIcon={<FontAwesomeIcon icon={faUsers} />}
                     />
                     <MenuItem
                         title={"Cập nhật thông tin cá nhân"}
                         to={config.routes.admin + "/updateProfile"}
-                        icon={""}
-                        activeIcon={""}
+                        icon={<FontAwesomeIcon icon={faUser} />}
+                        activeIcon={<FontAwesomeIcon icon={faUser} />}
                     />
                     <MenuItem
                         title={"Cập nhật thông tin xe"}
                         to={config.routes.admin + "/updateInfoMoto"}
-                        icon={""}
-                        activeIcon={""}
+                        icon={<FontAwesomeIcon icon={faMotorcycle} />}
+                        activeIcon={<FontAwesomeIcon icon={faMotorcycle} />}
                     />
                     <MenuItem
                         title={"Xác nhận đăng kí thuê xe"}
                         to={config.routes.admin + "/acceptRentMoto"}
-                        icon={""}
-                        activeIcon={""}
+                        icon={<FontAwesomeIcon icon={faCheckCircle} />}
+                        activeIcon={<FontAwesomeIcon icon={faCheckCircle} />}
                     />
                     <MenuItem
                         title={"Xác nhận trả xe"}
                         to={config.routes.admin + "/acceptReturnMoto"}
-                        icon={""}
-                        activeIcon={""}
+                        icon={<FontAwesomeIcon icon={faRotateLeft} />}
+                        activeIcon={<FontAwesomeIcon icon={faRotateLeft} />}
                     />
                 </Menu>
             </div>
